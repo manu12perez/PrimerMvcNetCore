b92625cbb7a03ce824333bf2fa4bb6b52e5775cf
manu12perez <manu12perez@gmail.com>
2025-01-28 10:15:16 +0100

    hola

--- PrimerMvcNetCore/Controllers/InformacionController.cs
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -9,7 +9,7 @@ namespace PrimerMvcNetCore.Controllers
         {
             return View();
         }
-        //mensaje de prueba
+        //mensaje de pruebas
 
         //VAMOS A RECIBIR DOS PARAMETROS
         public IActionResult VistaControllerGet
